8c3191f5a69fe50d8b90818d58bc1bfc22f3f3e0
Theoness1 <sasha.bolotskih2002@gmail.com>
2024-02-08 14:02:44 +0700

    Update Shop.cs

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Shop : MonoBehaviour
-{
+{//
     [SerializeField] private List<Weapon> _weapons;
     [SerializeField] private Player _player;
     [SerializeField] private WeaponView _template;
